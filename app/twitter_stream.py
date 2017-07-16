7f85e2348a81c98868640f3e77c9a0a006fe592a
Andrew Kenyon <andrewkenyon123@gmail.com>
2017-07-16 23:32:22 +0100

    -show retweets -tweets appears in reverse order -full text of tweet appears -name and profile image clickable

--- app/twitter_stream.py
+++ app/twitter_stream.py
@@ -12,41 +12,62 @@ access_token_secret = "SMyaN4ZQpGjq8ggMCDMGdsnj4eb2sLei8RDsuG7EYAVUw"
 from socketIO_client import SocketIO, LoggingNamespace
 socket = SocketIO('localhost', 5000, LoggingNamespace)
 
-#This is a basic listener that just prints received tweets to stdout.
+# This is a basic listener that just prints received tweets to stdout.
+
+
 class StdOutListener(StreamListener):
 
-    # def on_data(self, data):
-    #     print(data)
-    #     return True
+    def parse_tweet(self, tweet):
+        timestamp_ms = tweet.get('timestamp_ms', '')  # Quoted Status doesn't have a time stampe
+        user = tweet["user"]["screen_name"]
+        profile_image = tweet["user"]["profile_image_url_https"]
+        name = tweet["user"]["name"]
+        content = tweet["text"]
+        id = tweet["id_str"]
+        quoted_status = {}
+
+        # I don't know why but longer tweets are abbreviated in the main "text"
+        try:
+            content = tweet["extended_tweet"]["full_text"]
+        except KeyError:
+            pass
+
+        if tweet.get('quoted_status'):
+            quoted_status = self.parse_tweet(tweet['quoted_status'])
+
+        tweet = {'msg': content,
+                 'user': user,
+                 'timestamp': timestamp_ms,
+                 'id': id,
+                 'profile_image': profile_image,
+                 'name': name,
+                 'quoted_status': quoted_status,
+                 }
+
+        return tweet
 
     def on_data(self, json_data):
-        #TODO: How favorites?
+        # TODO: How favorites?
         data = json.loads(json_data)
-
         if data.get('retweeted_status'):
             # A retweet could be a retweet by an MP of or an MP
-            #print(timestamp, "This is a retweet")
+            # print(timestamp, "This is a retweet")
             pass
         elif data.get('in_reply_to_user_id'):
-            #print(timestamp, "This is a reply")
+            # print(timestamp, "This is a reply")
             pass
         else:
+            print("TWEET")
             try:
-                timestamp_ms = data['timestamp_ms']
-                timestamp = arrow.get(timestamp_ms[:-3])
-                user = data["user"]["screen_name"]
-                content = data["text"]
-                id = data["id_str"]
-                print("Timestamp: ", timestamp)
-                print("User: ", user)
-                print("Content: ", content)
-                print("ID: ", id)
+                message = self.parse_tweet(data)
+
                 socket.emit('text',
-                            {'msg': content, 'user': user, 'timestamp': timestamp_ms, 'id': id},
+                            message,
                             namespace='/',
                             broadcast=True)
-                #print(data)
-            except KeyError:
+            except KeyError as err:
+                print("Key error: {0}".format(err))
+                print("----------------------------")
                 print(data)
         return True
 
@@ -60,12 +81,12 @@ if __name__ == '__main__':
 
     twitter_ids = [str(member['twitter']) for member in data if member['twitter']]
 
-    #This handles Twitter authetification and the connection to Twitter Streaming API
+    # This handles Twitter authetification and the connection to Twitter Streaming API
     l = StdOutListener()
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     stream = Stream(auth, l)
 
-    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
-    #twitter_ids = ['15841632']
+    # This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
+    #twitter_ids = ['886603421410881537']
     stream.filter(follow= twitter_ids)
--- src/App.js
+++ src/App.js
@@ -12,8 +12,13 @@ class App extends Component {
         super(props);
         this.state = {
             fruits: [
-                {id: 'fruit-1', msg: 'apple'},
-                {id: 'fruit-2', msg: 'lemon'}
+                {
+                    id: 'fruit-1',
+                    msg: 'apple',
+                    user: "jessphillips",
+                    profile_image: "https://pbs.twimg.com/profile_images/597069124720828417/IvNyrUQc_bigger.jpg",
+                    name: "Jess Philips",
+                    quoted_status: {}}
             ]
         };
         this.addFruit = this.addFruit.bind(this);
@@ -44,7 +49,8 @@ class App extends Component {
         let timestamp = (new Date()).getTime();
         this.setState(function (state) {
             return {
-                fruits: state.fruits.concat(message)
+                // fruits: state.fruits.concat(message)
+                fruits: [message].concat(state.fruits)
             }
         })
     }
@@ -72,13 +78,46 @@ class App extends Component {
 let FruitList = React.createClass({
     render: function () {
         let stationComponents = this.props.fruits.map(function (tweet) {
-            return <li className="list-group-item list-group-item-info" key={tweet.id}><Linkify>{tweet.msg}</Linkify></li>
+            const has_quoted_status = Object.keys(tweet.quoted_status) != 0;
+            console.log(tweet);
+            return (
+                <div className="list-group-item list-group-item-info col-md-12" key={tweet.id}>
+                    <div className="col-md-1">
+                        <a href={"https://twitter.com/" + tweet.user}>
+                            <img className="user-image" src={tweet.profile_image} alt={tweet.user} width="48" height="48"/>
+                        </a>
+                    </div>
+                    <div className="col-md-11">
+                        <div className="stream-item header">
+                            <strong className="full-name"><a href={"https://twitter.com/" + tweet.user}>{tweet.name}</a></strong>
+                            <span className="user-name">@{tweet.user}</span>
+                        </div>
+                        <div className="stream-item body">
+                            <Linkify>{tweet.msg}</Linkify>
+                            {has_quoted_status ? (
+                                <div className="quoted-tweet-container">
+                                    <div className="quoted-tweet">
+                                        <div className="stream-item header">
+                                            <strong className="full-name"><a
+                                                href={"https://twitter.com/" + tweet.quoted_status.user}>{tweet.quoted_status.name}</a></strong>
+                                            <span className="user-name">@{tweet.quoted_status.user}</span>
+                                        </div>
+                                        <div className="stream-item body">
+                                            <Linkify>{tweet.quoted_status.msg}</Linkify>
+                                        </div>
+                                    </div>
+                                </div>
+                                ) : ("")}
+                        </div>
+                    </div>
+                </div>
+            )
         });
         return (
-            <div className="container">
-                <ul className="list-group text-center">
+            <div className="container ">
+                <div className="list-group text-center">
                     {stationComponents}
-                </ul>
+                </div>
             </div>);
     }
 });
